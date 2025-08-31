16b921296a552381ae4220b3da585922ec42098b
Shishupal-Yadav <shishupal614@gmail.com>
2025-08-31 17:16:13 +0530

    update-code

--- app-test.js
+++ app-test.js
@@ -1,27 +1,246 @@
+// const mongoose = require("mongoose");
+// const server = require("./app");
+// const chai = require("chai");
+// const chaiHttp = require("chai-http");
+
+
+// // Assertion 
+// chai.should();
+// chai.use(chaiHttp); 
+
+// const mongoUsername = process.env.MONGO_USERNAME || 'daemon4546_db_user';
+// const mongoPassword = process.env.MONGO_PASSWORD || '661NwD6qV63GxVzO';
+
+// let mongouri = process.env.MONGO_URI;
+// if (mongouri && mongouri.startsWith('mongodb://')) {
+//   console.warn('Warning: MONGO_URI is set in deprecated format. Using SRV fallback.');
+//   mongouri = null;
+// }
+// if (!mongouri) {
+//   mongouri = `mongodb+srv://${mongoUsername}:${mongoPassword}@solar-test-db.rusjxzi.mongodb.net/superdata?retryWrites=true&w=majority`;
+// }
+// console.log("Using MongoDB URI (test):", mongouri);
+
+// // Ensure ALL MongoDB connections in tests use mongouri
+// before(async () => {
+//   await mongoose.connect(mongouri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   });
+// });
+
+// describe('Sanity Check', () => {
+//   it('should run tests', () => {
+//     // If you see this in output, Mocha is working
+//   });
+// });
+
+// describe('Planets API Suite', () => {
+
+//     describe('Fetching Planet Details', () => {
+//         it('it should fetch a planet named Mercury', (done) => {
+//             let payload = {
+//                 id: 1
+//             }
+//           chai.request(server)
+//               .post('/planet')
+//               .send(payload)
+//               .end((err, res) => {
+//                     res.should.have.status(200);
+//                     res.body.should.have.property('id').eql(1);
+//                     res.body.should.have.property('name').eql('Mercury');
+//                 done();
+//               });
+//         });
+
+//         it('it should fetch a planet named Venus', (done) => {
+//             let payload = {
+//                 id: 2
+//             }
+//           chai.request(server)
+//               .post('/planet')
+//               .send(payload)
+//               .end((err, res) => {
+//                     res.should.have.status(200);
+//                     res.body.should.have.property('id').eql(2);
+//                     res.body.should.have.property('name').eql('Venus');
+//                 done();
+//               });
+//         });
+
+//         it('it should fetch a planet named Earth', (done) => {
+//             let payload = {
+//                 id: 3
+//             }
+//           chai.request(server)
+//               .post('/planet')
+//               .send(payload)
+//               .end((err, res) => {
+//                     res.should.have.status(200);
+//                     res.body.should.have.property('id').eql(3);
+//                     res.body.should.have.property('name').eql('Earth');
+//                 done();
+//               });
+//         });
+//         it('it should fetch a planet named Mars', (done) => {
+//             let payload = {
+//                 id: 4
+//             }
+//           chai.request(server)
+//               .post('/planet')
+//               .send(payload)
+//               .end((err, res) => {
+//                     res.should.have.status(200);
+//                     res.body.should.have.property('id').eql(4);
+//                     res.body.should.have.property('name').eql('Mars');
+//                 done();
+//               });
+//         });
+
+//         it('it should fetch a planet named Jupiter', (done) => {
+//             let payload = {
+//                 id: 5
+//             }
+//           chai.request(server)
+//               .post('/planet')
+//               .send(payload)
+//               .end((err, res) => {
+//                     res.should.have.status(200);
+//                     res.body.should.have.property('id').eql(5);
+//                     res.body.should.have.property('name').eql('Jupiter');
+//                 done();
+//               });
+//         });
+
+//         it('it should fetch a planet named Satrun', (done) => {
+//             let payload = {
+//                 id: 6
+//             }
+//           chai.request(server)
+//               .post('/planet')
+//               .send(payload)
+//               .end((err, res) => {
+//                     res.should.have.status(200);
+//                     res.body.should.have.property('id').eql(6);
+//                     res.body.should.have.property('name').eql('Saturn');
+//                 done();
+//               });
+//         });
+
+//         it('it should fetch a planet named Uranus', (done) => {
+//             let payload = {
+//                 id: 7
+//             }
+//           chai.request(server)
+//               .post('/planet')
+//               .send(payload)
+//               .end((err, res) => {
+//                     res.should.have.status(200);
+//                     res.body.should.have.property('id').eql(7);
+//                     res.body.should.have.property('name').eql('Uranus');
+//                 done();
+//               });
+//         });
+
+//         it('it should fetch a planet named Neptune', (done) => {
+//             let payload = {
+//                 id: 8
+//             }
+//           chai.request(server)
+//               .post('/planet')
+//               .send(payload)
+//               .end((err, res) => {
+//                     res.should.have.status(200);
+//                     res.body.should.have.property('id').eql(8);
+//                     res.body.should.have.property('name').eql('Neptune');
+//                 done();
+//               });
+//         });
+
+//         // it('it should fetch a planet named Pluto', (done) => {
+//         //     let payload = {
+//         //         id: 9
+//         //     }
+//         //   chai.request(server)
+//         //       .post('/planet')
+//         //       .send(payload)
+//         //       .end((err, res) => {
+//         //             res.should.have.status(200);
+//         //             res.body.should.have.property('id').eql(9);
+//         //             res.body.should.have.property('name').eql('Sun');
+//         //         done();
+//         //       });
+//         // });
+
+
+//     });        
+// });
+
+// //Use below test case to achieve coverage
+// describe('Testing Other Endpoints', () => {
+
+//     describe('it should fetch OS Details', () => {
+//         it('it should fetch OS details', (done) => {
+//           chai.request(server)
+//               .get('/os')
+//               .end((err, res) => {
+//                     res.should.have.status(200);
+//                 done();
+//               });
+//         });
+//     });
+
+//     describe('it should fetch Live Status', () => {
+//         it('it checks Liveness endpoint', (done) => {
+//           chai.request(server)
+//               .get('/live')
+//               .end((err, res) => {
+//                     res.should.have.status(200);
+//                     res.body.should.have.property('status').eql('live');
+//                 done();
+//               });
+//         });
+//     });
+
+//     describe('it should fetch Ready Status', () => {
+//         it('it checks Readiness endpoint', (done) => {
+//           chai.request(server)
+//               .get('/ready')
+//               .end((err, res) => {
+//                     res.should.have.status(200);
+//                     res.body.should.have.property('status').eql('ready');
+//                 done();
+//               });
+//         });
+//     });
+
+// });
+
+// after(async () => {
+//   await mongoose.disconnect();
+//   console.log("All tests finished.");
+// });
+
 const mongoose = require("mongoose");
-const server = require("./app");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+require("dotenv").config();
 
-
-// Assertion 
+const server = require("./app");
 chai.should();
-chai.use(chaiHttp); 
+chai.use(chaiHttp);
 
-const mongoUsername = process.env.MONGO_USERNAME || 'daemon4546_db_user';
-const mongoPassword = process.env.MONGO_PASSWORD || '661NwD6qV63GxVzO';
-
+// Prefer full MONGO_URI, fallback to username/password
 let mongouri = process.env.MONGO_URI;
-if (mongouri && mongouri.startsWith('mongodb://')) {
-  console.warn('Warning: MONGO_URI is set in deprecated format. Using SRV fallback.');
-  mongouri = null;
+if (!mongouri && process.env.MONGO_USERNAME && process.env.MONGO_PASSWORD) {
+  mongouri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@solar-test-db.rusjxzi.mongodb.net/superdata?retryWrites=true&w=majority`;
 }
 if (!mongouri) {
-  mongouri = `mongodb+srv://${mongoUsername}:${mongoPassword}@solar-test-db.rusjxzi.mongodb.net/superdata?retryWrites=true&w=majority`;
+  throw new Error("❌ No MongoDB connection string found. Please set MONGO_URI in .env");
 }
+
 console.log("Using MongoDB URI (test):", mongouri);
 
-// Ensure ALL MongoDB connections in tests use mongouri
 before(async () => {
   await mongoose.connect(mongouri, {
     useNewUrlParser: true,
@@ -29,194 +248,71 @@ before(async () => {
   });
 });
 
-describe('Sanity Check', () => {
-  it('should run tests', () => {
-    // If you see this in output, Mocha is working
+describe("Sanity Check", () => {
+  it("should run tests", () => {});
+});
+
+describe("Planets API Suite", () => {
+  describe("Fetching Planet Details", () => {
+    const testCases = [
+      { id: 1, name: "Mercury" },
+      { id: 2, name: "Venus" },
+      { id: 3, name: "Earth" },
+      { id: 4, name: "Mars" },
+      { id: 5, name: "Jupiter" },
+      { id: 6, name: "Saturn" },
+      { id: 7, name: "Uranus" },
+      { id: 8, name: "Neptune" }
+    ];
+
+    testCases.forEach(tc => {
+      it(`should fetch planet ${tc.name}`, (done) => {
+        chai.request(server)
+          .post("/planet")
+          .send({ id: tc.id })
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.have.property("id").eql(tc.id);
+            res.body.should.have.property("name").eql(tc.name);
+            done();
+          });
+      });
+    });
   });
 });
 
-describe('Planets API Suite', () => {
+describe("Testing Other Endpoints", () => {
+  it("should fetch OS details", (done) => {
+    chai.request(server)
+      .get("/os")
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
 
-    describe('Fetching Planet Details', () => {
-        it('it should fetch a planet named Mercury', (done) => {
-            let payload = {
-                id: 1
-            }
-          chai.request(server)
-              .post('/planet')
-              .send(payload)
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('id').eql(1);
-                    res.body.should.have.property('name').eql('Mercury');
-                done();
-              });
-        });
+  it("should check liveness", (done) => {
+    chai.request(server)
+      .get("/live")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property("status").eql("live");
+        done();
+      });
+  });
 
-        it('it should fetch a planet named Venus', (done) => {
-            let payload = {
-                id: 2
-            }
-          chai.request(server)
-              .post('/planet')
-              .send(payload)
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('id').eql(2);
-                    res.body.should.have.property('name').eql('Venus');
-                done();
-              });
-        });
-
-        it('it should fetch a planet named Earth', (done) => {
-            let payload = {
-                id: 3
-            }
-          chai.request(server)
-              .post('/planet')
-              .send(payload)
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('id').eql(3);
-                    res.body.should.have.property('name').eql('Earth');
-                done();
-              });
-        });
-        it('it should fetch a planet named Mars', (done) => {
-            let payload = {
-                id: 4
-            }
-          chai.request(server)
-              .post('/planet')
-              .send(payload)
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('id').eql(4);
-                    res.body.should.have.property('name').eql('Mars');
-                done();
-              });
-        });
-
-        it('it should fetch a planet named Jupiter', (done) => {
-            let payload = {
-                id: 5
-            }
-          chai.request(server)
-              .post('/planet')
-              .send(payload)
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('id').eql(5);
-                    res.body.should.have.property('name').eql('Jupiter');
-                done();
-              });
-        });
-
-        it('it should fetch a planet named Satrun', (done) => {
-            let payload = {
-                id: 6
-            }
-          chai.request(server)
-              .post('/planet')
-              .send(payload)
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('id').eql(6);
-                    res.body.should.have.property('name').eql('Saturn');
-                done();
-              });
-        });
-
-        it('it should fetch a planet named Uranus', (done) => {
-            let payload = {
-                id: 7
-            }
-          chai.request(server)
-              .post('/planet')
-              .send(payload)
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('id').eql(7);
-                    res.body.should.have.property('name').eql('Uranus');
-                done();
-              });
-        });
-
-        it('it should fetch a planet named Neptune', (done) => {
-            let payload = {
-                id: 8
-            }
-          chai.request(server)
-              .post('/planet')
-              .send(payload)
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('id').eql(8);
-                    res.body.should.have.property('name').eql('Neptune');
-                done();
-              });
-        });
-
-        // it('it should fetch a planet named Pluto', (done) => {
-        //     let payload = {
-        //         id: 9
-        //     }
-        //   chai.request(server)
-        //       .post('/planet')
-        //       .send(payload)
-        //       .end((err, res) => {
-        //             res.should.have.status(200);
-        //             res.body.should.have.property('id').eql(9);
-        //             res.body.should.have.property('name').eql('Sun');
-        //         done();
-        //       });
-        // });
-
-
-    });        
-});
-
-//Use below test case to achieve coverage
-describe('Testing Other Endpoints', () => {
-
-    describe('it should fetch OS Details', () => {
-        it('it should fetch OS details', (done) => {
-          chai.request(server)
-              .get('/os')
-              .end((err, res) => {
-                    res.should.have.status(200);
-                done();
-              });
-        });
-    });
-
-    describe('it should fetch Live Status', () => {
-        it('it checks Liveness endpoint', (done) => {
-          chai.request(server)
-              .get('/live')
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('status').eql('live');
-                done();
-              });
-        });
-    });
-
-    describe('it should fetch Ready Status', () => {
-        it('it checks Readiness endpoint', (done) => {
-          chai.request(server)
-              .get('/ready')
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('status').eql('ready');
-                done();
-              });
-        });
-    });
-
+  it("should check readiness", (done) => {
+    chai.request(server)
+      .get("/ready")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.have.property("status").eql("ready");
+        done();
+      });
+  });
 });
 
 after(async () => {
   await mongoose.disconnect();
-  console.log("All tests finished.");
+  console.log("✅ All tests finished.");
 });
--- app.js
+++ app.js
@@ -1,104 +1,298 @@
+// const path = require('path');
+// const express = require('express');
+// const OS = require('os');
+// const bodyParser = require('body-parser');
+// const mongoose = require("mongoose");
+// const app = express();
+// const cors = require('cors')
+
+// // Use environment variables for MongoDB credentials if available
+// const mongoUsername = process.env.MONGO_USERNAME || 'daemon4546_db_user';
+// const mongoPassword = process.env.MONGO_PASSWORD || '661NwD6qV63GxVzO';
+
+// let mongouri = process.env.MONGO_URI;
+
+// // If MONGO_URI is set and starts with 'mongodb://', ignore it and use fallback
+// if (mongouri && mongouri.startsWith('mongodb://')) {
+//   console.warn('Warning: MONGO_URI is set in deprecated format. Using SRV fallback.');
+//   mongouri = null;
+// }
+
+// // If not set, use SRV format fallback
+// if (!mongouri) {
+//   mongouri = `mongodb+srv://${mongoUsername}:${mongoPassword}@solar-test-db.rusjxzi.mongodb.net/superdata?retryWrites=true&w=majority`;
+// }
+
+// // Log the final MongoDB URI for debugging
+// console.log("Using MongoDB URI:", mongouri);
+
+// app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, '/')));
+// app.use(cors())
+// async function startServer() {
+//   try {
+//     await mongoose.connect(mongouri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     });
+//     console.log("MongoDB Connection Successful");
+//     app.listen(3000, () => {
+//       console.log("Server successfully running on port - " + 3000);
+//     });
+//   } catch (err) {
+//     console.log("error!! " + err);
+//     process.exit(1);
+//   }
+// }
+
+// startServer();
+
+// var Schema = mongoose.Schema;
+
+// var dataSchema = new Schema({
+//     name: String,
+//     id: Number,
+//     description: String,
+//     image: String,
+//     velocity: String,
+//     distance: String
+// });
+// var planetModel = mongoose.model('planets', dataSchema);
+
+
+
+// app.post('/planet', async function(req, res) {
+//     try {
+//         const planetData = await planetModel.findOne({ id: req.body.id });
+//         if (!planetData) {
+//             res.status(404).send("Planet not found");
+//         } else {
+//             res.send(planetData);
+//         }
+//     } catch (err) {
+//         res.status(500).send("Error in Planet Data");
+//     }
+// })
+
+// app.get('/',   async (req, res) => {
+//     res.sendFile(path.join(__dirname, '/', 'index.html'));
+// });
+
+
+// app.get('/os',   function(req, res) {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send({
+//         "os": OS.hostname(),
+//         "env": process.env.NODE_ENV
+//     });
+// })
+
+// app.get('/live',   function(req, res) {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send({
+//         "status": "live"
+//     });
+// })
+
+// app.get('/ready',   function(req, res) {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send({
+//         "status": "ready"
+//     });
+// })
+
+// module.exports = app;
+
+
+
+
+
+
+// const path = require('path');
+// const express = require('express');
+// const OS = require('os');
+// const bodyParser = require('body-parser');
+// const mongoose = require("mongoose");
+// const cors = require('cors');
+// require('dotenv').config();
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// // Prefer full MONGO_URI, fallback to building from username/password
+// let mongouri = process.env.MONGO_URI;
+// if (!mongouri && process.env.MONGO_USERNAME && process.env.MONGO_PASSWORD) {
+//   mongouri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@solar-test-db.rusjxzi.mongodb.net/superdata?retryWrites=true&w=majority`;
+// }
+
+// if (!mongouri) {
+//   console.error("❌ No MongoDB connection string found. Please set MONGO_URI in .env");
+//   process.exit(1);
+// }
+
+// console.log("Using MongoDB URI:", mongouri);
+
+// app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, '/')));
+// app.use(cors());
+
+// async function startServer() {
+//   try {
+//     await mongoose.connect(mongouri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     });
+//     console.log("✅ MongoDB Connection Successful");
+//     app.listen(PORT, () => {
+//       console.log(`🚀 Server running on port ${PORT}`);
+//     });
+//   } catch (err) {
+//     console.log("❌ MongoDB connection error:", err);
+//     process.exit(1);
+//   }
+// }
+// startServer();
+
+// // Schema & model
+// const Schema = mongoose.Schema;
+// const dataSchema = new Schema({
+//   name: String,
+//   id: Number,
+//   description: String,
+//   image: String,
+//   velocity: String,
+//   distance: String
+// });
+// const planetModel = mongoose.model('planets', dataSchema);
+
+// // Routes
+// app.post('/planet', async function(req, res) {
+//   try {
+//     const planetData = await planetModel.findOne({ id: req.body.id });
+//     if (!planetData) {
+//       res.status(404).send("Planet not found");
+//     } else {
+//       res.send(planetData);
+//     }
+//   } catch (err) {
+//     res.status(500).send("Error in Planet Data");
+//   }
+// });
+
+// app.get('/', async (req, res) => {
+//   res.sendFile(path.join(__dirname, '/', 'index.html'));
+// });
+
+// app.get('/os', function(req, res) {
+//   res.json({
+//     os: OS.hostname(),
+//     env: process.env.NODE_ENV || "development"
+//   });
+// });
+
+// app.get('/live', function(req, res) {
+//   res.json({ status: "live" });
+// });
+
+// app.get('/ready', function(req, res) {
+//   res.json({ status: "ready" });
+// });
+
+// module.exports = app;
+
+
 const path = require('path');
 const express = require('express');
 const OS = require('os');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
-const cors = require('cors')
+const PORT = process.env.PORT || 3000;
 
-// Use environment variables for MongoDB credentials if available
-const mongoUsername = process.env.MONGO_USERNAME || 'daemon4546_db_user';
-const mongoPassword = process.env.MONGO_PASSWORD || '661NwD6qV63GxVzO';
+// Load env vars
+const {
+  MONGO_URI,
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  CLUSTER_NAME,
+  DB_NAME
+} = process.env;
 
-let mongouri = process.env.MONGO_URI;
-
-// If MONGO_URI is set and starts with 'mongodb://', ignore it and use fallback
-if (mongouri && mongouri.startsWith('mongodb://')) {
-  console.warn('Warning: MONGO_URI is set in deprecated format. Using SRV fallback.');
-  mongouri = null;
+// Prefer full URI, otherwise build from parts
+let mongoUri = MONGO_URI;
+if (!mongoUri && MONGO_USERNAME && MONGO_PASSWORD && CLUSTER_NAME && DB_NAME) {
+  mongoUri = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${CLUSTER_NAME}.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 }
 
-// If not set, use SRV format fallback
-if (!mongouri) {
-  mongouri = `mongodb+srv://${mongoUsername}:${mongoPassword}@solar-test-db.rusjxzi.mongodb.net/superdata?retryWrites=true&w=majority`;
+if (!mongoUri) {
+  console.error("❌ No MongoDB URI found. Please set MONGO_URI or individual parts in .env");
+  process.exit(1);
 }
 
-// Log the final MongoDB URI for debugging
-console.log("Using MongoDB URI:", mongouri);
+// Log safe info (do not print credentials)
+console.log("Using MongoDB cluster:", mongoUri.replace(/\/\/.*@/, "//<credentials>@"));
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
-app.use(cors())
+app.use(cors());
+
+// Connect DB and start server
 async function startServer() {
   try {
-    await mongoose.connect(mongouri, {
+    await mongoose.connect(mongoUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    console.log("MongoDB Connection Successful");
-    app.listen(3000, () => {
-      console.log("Server successfully running on port - " + 3000);
+    console.log("✅ MongoDB connected successfully");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
     });
   } catch (err) {
-    console.log("error!! " + err);
+    console.error("❌ MongoDB connection error:", err.message);
     process.exit(1);
   }
 }
-
 startServer();
 
-var Schema = mongoose.Schema;
-
-var dataSchema = new Schema({
-    name: String,
-    id: Number,
-    description: String,
-    image: String,
-    velocity: String,
-    distance: String
+// Schema & model
+const planetSchema = new mongoose.Schema({
+  name: String,
+  id: Number,
+  description: String,
+  image: String,
+  velocity: String,
+  distance: String
 });
-var planetModel = mongoose.model('planets', dataSchema);
+const Planet = mongoose.model('Planet', planetSchema);
 
-
-
-app.post('/planet', async function(req, res) {
-    try {
-        const planetData = await planetModel.findOne({ id: req.body.id });
-        if (!planetData) {
-            res.status(404).send("Planet not found");
-        } else {
-            res.send(planetData);
-        }
-    } catch (err) {
-        res.status(500).send("Error in Planet Data");
+// Routes
+app.post('/planet', async (req, res) => {
+  try {
+    const planet = await Planet.findOne({ id: req.body.id }).exec();
+    if (!planet) {
+      return res.status(404).json({ error: "Planet not found" });
     }
-})
-
-app.get('/',   async (req, res) => {
-    res.sendFile(path.join(__dirname, '/', 'index.html'));
+    res.json(planet);
+  } catch (err) {
+    res.status(500).json({ error: "Error fetching planet data", details: err.message });
+  }
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/', 'index.html'));
+});
 
-app.get('/os',   function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
-        "os": OS.hostname(),
-        "env": process.env.NODE_ENV
-    });
-})
+app.get('/os', (req, res) => {
+  res.json({
+    os: OS.hostname(),
+    env: process.env.NODE_ENV || "development"
+  });
+});
 
-app.get('/live',   function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
-        "status": "live"
-    });
-})
-
-app.get('/ready',   function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
-        "status": "ready"
-    });
-})
+app.get('/live', (req, res) => res.json({ status: "live" }));
+app.get('/ready', (req, res) => res.json({ status: "ready" }));
 
 module.exports = app;
